250be77f83e88ab52b6eac0a7a3aa91ce8eef81c
Ryusei <ryusei1597@gmail.com>
2021-02-26 17:24:04 +0900

    initial commit

---
+++ cross_book/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CrossBookConfig(AppConfig):
+    name = 'cross_book'
---
+++ cross_book/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import User
+
+
+class MyUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username','email',)#
+
+
