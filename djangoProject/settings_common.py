c4cc77a9bc3d635659b9a524929d998c3168a229
Ryusei <ryusei1597@gmail.com>
2021-07-06 15:00:22 +0900

    modify staticfiles_dirs

--- djangoProject/settings_common.py
+++ djangoProject/settings_common.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR, 'static',
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 
--- djangoProject/settings_dev.py
+++ djangoProject/settings_dev.py
@@ -53,3 +53,4 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_dev')
